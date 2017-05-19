591634bc2027ef908657ce159372c39a8da5fbf0
leoseven <leolovecode@163.com>
2017-05-19 23:36:23 +0800

    xx

--- music/src/main/java/com/gdm/service/CommentService.java
+++
@@ -1,9 +0,0 @@
-package com.gdm.service;
-
-import com.gdm.model.Comment;
-
-public interface CommentService {
-	boolean addcomment(Comment c);
-	Comment getComment(Integer id);
-	boolean updateComment(Comment c);
-}
